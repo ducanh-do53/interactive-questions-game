a3b4dac38ca5f22779a1bd57f85e1aaab2234262
Duc Anh Do <anhducdo53@gmail.com>
2021-10-24 03:41:07 +0700

    Editing javascript files

---
+++ main.js
@@ -0,0 +1,12 @@
+function generateNumber(max) {
+    return Math.floor(Math.random() * (max + 1))
+}
+
+function generateProblem() {
+    return {
+        numberOne: generateNumber(10),
+        numberTwo: generateNumber(10),
+        operator: ['+', '-', 'x'] [generateNumber(2)]
+    }
+}
+
